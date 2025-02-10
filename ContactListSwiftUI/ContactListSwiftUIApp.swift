c480b03a2e16f4473b85e04a5d4b574e99c52eec
AnastasyiaMaximova <rsk.maximova@bk.ru>
2025-02-10 20:07:30 +0300

    Initial Commit

---
+++ ContactListSwiftUI/ContactListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIApp.swift
+//  ContactListSwiftUI
+//
+//  Created by Anastasya Maximova on 10.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
